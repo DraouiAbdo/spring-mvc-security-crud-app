c66118cbe32cb03aa90fb06e76e68fd8e83ed308
abdelghani draoui <draoui.abdelghani@gmail.com>
2023-04-12 10:35:29 +0200

    switched from in-memory users creation to in db users creation

--- src/main/java/dz/cut/springmvcsimpleapp/SpringMvcSimpleAppApplication.java
+++ src/main/java/dz/cut/springmvcsimpleapp/SpringMvcSimpleAppApplication.java
@@ -24,13 +24,18 @@ public class SpringMvcSimpleAppApplication {
 
 
             Student s1 = new Student(null,"Nasro",new Date(),true,50);
-
             Student s2 = new Student(null,"Omar",new Date(),false,20);
             Student s3 = new Student(null,"Youcef",new Date(),true,80);
+            Student s4 = new Student(null,"Amine",new Date(),true,50);
+            Student s5 = new Student(null,"Khaled",new Date(),false,20);
+            Student s6 = new Student(null,"Aicha",new Date(),true,80);
 
-//            studentService.addNewStudent(s1);
-//            studentService.addNewStudent(s2);
-//            studentService.addNewStudent(s3);
+            studentService.addNewStudent(s1);
+            studentService.addNewStudent(s2);
+            studentService.addNewStudent(s3);
+            studentService.addNewStudent(s4);
+            studentService.addNewStudent(s5);
+            studentService.addNewStudent(s6);
 
 //            Student s4 = studentService.findStudentByNameContains("ma");
 //            System.out.println(s4.getName()+" rank is "+s4.getRank());
--- src/main/java/dz/cut/springmvcsimpleapp/sec/SecurityConfig.java
+++ src/main/java/dz/cut/springmvcsimpleapp/sec/SecurityConfig.java
@@ -1,5 +1,6 @@
 package dz.cut.springmvcsimpleapp.sec;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -9,17 +10,33 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @Configuration @EnableWebSecurity
 public class SecurityConfig  extends WebSecurityConfigurerAdapter {
 
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        PasswordEncoder p  = passwordEncoder();
+//        String EncodedPWD1 = p.encode("123");
+//        auth.inMemoryAuthentication().withUser("user1")
+//                .password(EncodedPWD1).roles("USER");
+//        auth.inMemoryAuthentication().withUser("admin")
+//                .password(p.encode("456")).roles("USER","ADMIN");
+//    }
+
+    @Autowired
+    private DataSource dataSource;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username as principal, password as credentials, active from users where username=?")
+                .authoritiesByUsernameQuery("select username as principal, role as user_roles from users_roles where username = ?")
+                .rolePrefix("ROLE_");
         PasswordEncoder p  = passwordEncoder();
-        String EncodedPWD1 = p.encode("123");
-        auth.inMemoryAuthentication().withUser("user1")
-                .password(EncodedPWD1).roles("USER");
-        auth.inMemoryAuthentication().withUser("admin")
-                .password(p.encode("456")).roles("USER","ADMIN");
+
     }
 
     @Override
